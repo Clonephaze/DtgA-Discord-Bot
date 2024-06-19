7a199295727970f31ecfbb45f082e2639f6a80ef
Clonephaze <jackdsmith2327@gmail.com>
2024-06-19 10:09:43 -0700

    Update Bot Start.bat

--- Bot Start.bat
+++ Bot Start.bat
@@ -8,13 +8,16 @@ git fetch
 :: Check if there are any updates
 for /f %%i in ('git rev-list HEAD...origin/main --count') do set BEHIND=%%i
 
+:: If there are updates, pull them
 if %BEHIND% GTR 0 (
     echo Pulling latest updates...
     git pull
+:: If there are no updates, echo it and move on
 ) else (
     echo No updates found.
 )
 
+:: Log the bot in
 echo Logging in...
 
 :: Launch the Node.js application
